1e4dc599a62fd9158729e83b83f56ac42a06bc7e
unknown <cehrett@gmail.com>
2020-04-14 14:49:58 -0400

    aligning_times.R now requires less memory
    Previously, aligning_times.R loaded all player information into memory
    simultaneously, in order to align the times. It has been changed to be
    more parsimonious. There is a variable, max_chunk_size, which sets the
    maximum number of individual player csvs to load into memory at one
    time. The script writes a number of aligned csvs, each with
    max_chunk_size players in it (or fewer). Afterward, it attempts to
    combine all these csvs and writes the resulting master csv. This last
    part -- combining the few csvs into one master csv -- is fairly memory
    intensive, and thus it is possible that the script will fail here even
    if it succeeds in making the csvs that each have max_chunk_size players
    in them. If necessary, those csvs can be used/combined in some other
    way.

--- aligning_times.R
+++ aligning_times.R
@@ -12,27 +12,36 @@ setwd('C:/Users/Carl/Documents/R/project_football')
 
 data_files <- list.files('TAM/', pattern='*.csv', full.names = TRUE)
 
-# read first line with reference time and convert into a time format (UTC timezone)
-con <- file(data_files[1], 'r')
-line <- readLines(con, n=1)
-close(con)
-rm(con)
 
-time <- strsplit(line, ': ')[[1]][2]
-time_ld <- lubridate::mdy_hms(time)
-str(time_ld)
+#################################
+# THIS SECTION JUST FOR TESTING #
+#################################
 
-# read in datafile and shift all times by reference time
-df <- readr::read_csv(data_files[1], skip=3, col_types = paste('t',paste(rep('d',26),collapse = ""),sep=''))
-# warning: may be losing data. See parsing failures in readr output
-# df_copy <- df
-# df <- df_copy
+# # read first line with reference time and convert into a time format (UTC timezone)
+# con <- file(data_files[1], 'r')
+# line <- readLines(con, n=1)
+# close(con)
+# rm(con)
+# 
+# time <- strsplit(line, ': ')[[1]][2]
+# time_ld <- lubridate::mdy_hms(time)
+# str(time_ld)
+# 
+# # read in datafile and shift all times by reference time
+# df <- readr::read_csv(data_files[1], skip=3, col_types = paste('t',paste(rep('d',26),collapse = ""),sep=''))
+# # warning: may be losing data. See parsing failures in readr output
+# # df_copy <- df
+# # df <- df_copy
+# 
+# df <- df %>% mutate(TimeStamp = time_ld + TimeStamp)
+# 
+# df %>% head() %>% View()
+# # note: may be losing data for hundredths of seconds
+# # may be better to use centiseconds
 
-df <- df %>% mutate(TimeStamp = time_ld + TimeStamp)
-
-df %>% head() %>% View()
-# note: may be losing data for hundredths of seconds
-# may be better to use centiseconds
+##################################
+# ABOVE SECTION JUST FOR TESTING #
+##################################
 
 
 ##
@@ -59,39 +68,69 @@ fn_readPlayerData <- function(full_path){
   return(df)
 }
 
-fn_readPlayerData(data_files[1]) %>% head() %>% View()
+# fn_readPlayerData(data_files[1]) %>% head() %>% View()
+
+# Running into memory overload issues. Have to split the data files into chunks.
+max_chunk_size = 8 #### MODIFY: LARGER = FASTER, BUT TAKES MORE MEMORY. IF MEMORY FAILURE OCCURS, LOWER.
+data_files_chunked <- split(data_files, ceiling(seq_along(data_files)/max_chunk_size))
+dfc_length <- length(data_files_chunked)
+aligned_player_velocity_csv_filenames <- NULL
+aligned_player_velocity_playercounts <- NULL
 
 # apply function to all of the data
-df_all <- plyr::ldply(data_files, fn_readPlayerData)
-
-##
-## reshape the data with players as columns
-
-# split - apply - combine workflow:
-# split: group to unique player, second
-# apply: take the average velocity
-# combine: create a new dataframe 
-df_all_agg <-
-  df_all %>%
-  filter(!is.na(RawVelocity)) %>%
-  dplyr::group_by(TimeStamp=lubridate::ymd_hms(TimeStamp), player) %>%
-  dplyr::summarise(
-    mean_velocity = mean(RawVelocity, na.rm=TRUE),
-#    mean_heart_rate = mean(HeartRate)
-  )
-
-# turn single player column into multiple columns
-df_wide <- 
-  df_all_agg %>%
-  melt(id.vars = c('TimeStamp', 'player')) %>% 
-  dcast(TimeStamp ~ player + variable)
-
-readr::write_csv(df_wide, "aligned_player_velocities.csv")
-
-df_all_agg %>% 
-  group_by(TimeStamp) %>%
-  summarize(mean_velocity = mean(mean_velocity, na.rm = T)) %>%
-  ggplot(aes(x=TimeStamp, y=mean_velocity)) +
-  geom_point()
+for (i in seq_along(data_files_chunked)) {
+  chunk <- data_files_chunked[[i]]
+  length_of_chunk <- length(chunk)
+  df_all <- plyr::ldply(chunk, fn_readPlayerData)
   
+  ##
+  ## reshape the data with players as columns
+  
+  # split - apply - combine workflow:
+  # split: group to unique player, second
+  # apply: take the average velocity
+  # combine: create a new dataframe 
+  df_all_agg <-
+    df_all %>%
+    filter(!is.na(RawVelocity)) %>%
+    dplyr::group_by(TimeStamp=lubridate::ymd_hms(TimeStamp), player) %>%
+    dplyr::summarise(
+      mean_velocity = mean(RawVelocity, na.rm=TRUE),
+      #    mean_heart_rate = mean(HeartRate)
+    )
+  rm(df_all)
+  
+  # turn single player column into multiple columns
+  df_wide <- 
+    df_all_agg %>%
+    melt(id.vars = c('TimeStamp', 'player')) %>% 
+    dcast(TimeStamp ~ player + variable)
+  rm(df_all_agg)
+  
+  # Write processed chunk to csv
+  csv_name <- paste('aligned_player_velocities_chunk_',i,'_of_',dfc_length,sep='')
+  readr::write_csv(df_wide, csv_name)
+  rm(df_wide)
+  
+  # Record the name of the csv
+  aligned_player_velocity_csv_filenames <- c(aligned_player_velocity_csv_filenames,csv_name)
+  aligned_player_velocity_playercounts <- c(aligned_player_velocity_playercounts,length_of_chunk)
+}
 
+
+# Now try to combine them (might have memory problems here again):
+csv_name <- aligned_player_velocity_csv_filenames[1]
+csv_size <- aligned_player_velocity_playercounts[1]
+df <- readr::read_csv(csv_name, 
+                      col_types = paste('T',paste(rep('d',csv_size),collapse = ""),sep=''))
+while (length(aligned_player_velocity_csv_filenames) > 1) {
+  aligned_player_velocity_csv_filenames <- aligned_player_velocity_csv_filenames[-1]
+  aligned_player_velocity_playercounts <- aligned_player_velocity_playercounts[-1]
+  df <- merge(x=df, 
+              y=readr::read_csv(
+                aligned_player_velocity_csv_filenames[1],
+                col_types = paste('T',paste(rep('d',aligned_player_velocity_playercounts[1]),
+                                            collapse = ""),sep='')),
+              by="TimeStamp",all=TRUE)
+}
+readr::write_csv(df, 'aligned_player_velocities')
